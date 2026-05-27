9ddc1abb539dcfb454ca484d61e723e2256b7a9d
ANDY1754 <anderson.farinango@upec.edu.ec>
2026-05-27 23:40:33 +0000

    ejercicio_01 python

---
+++ ejercicio_01.py
@@ -0,0 +1,29 @@
+# Mi primer programa en python
+# Programa que Calcula la suma de tres numeros y su promedio
+numero_1 = 20
+numero_2 = 30
+numero_3 = 40 
+suma = numero_1 + numero_2 + numero_3
+print("la suma de los tres numeros es: ", suma)
+promedio = suma /3
+print ("el promedio de los tres numeros es: ", promedio)
+# Verificar si los números son pares o impares
+
+numero_1 = 20
+numero_2 = 30
+numero_3 = 41
+
+if numero_1 % 2 == 0:
+    print("El numero 1 es par")
+else:
+    print("El numero 1 es impar")
+
+if numero_2 % 2 == 0:
+    print("El numero 2 es par")
+else:
+    print("El numero 2 es impar")
+
+if numero_3 % 2 == 0:
+    print("El numero 3 es par")
+else:
+    print("El numero 3 es impar")
